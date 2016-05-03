dd5aac85019d9477f75a04a9c421542a9688aa27
FlorentinDUBOIS <dubois.florentin@live.fr>
2016-05-03 17:46:49 +0200

    udpate

--- app.js
+++ app.js
@@ -3,11 +3,27 @@
 var portfolio = angular.module( 'portfolio', ['ngAria', 'ngRoute', 'ngSanitize']);
 
 // ----------------------------------------------------------------------------
-// controller head
+// controller
 portfolio.controller( 'head', ['$scope', '$http', function( $scope, $http ) {
     $http.get( 'portfolio/banner' ).then( function( res ) {
-        if( res == 200 ) {
-            $scope.title = res.data.title;
+        if( res.status == 200 ) {
+            $scope.banner = res.data;
+        }
+    });
+}]);
+
+// ----------------------------------------------------------------------------
+// controller
+portfolio.controller( 'banner', ['$scope', '$http', function( $scope, $http ) {
+    $http.get( 'user/public_name' ).then( function( res ) {
+        if( res.status == 200 ) {
+            $scope.name = res.data;
+        }
+    });
+
+    $http.get( 'portfolio/banner' ).then( function( res ) {
+        if( res.status == 200 ) {
+            $scope.banner = res.data;
         }
     });
 }]);
