932151b0c2a6ab294ee8121e9075e15828a98a8b
FlorentinDUBOIS <dubois.florentin@live.fr>
2016-05-03 16:42:04 +0200

    change app

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // ----------------------------------------------------------------------------
 // create default template
-var template = angular.module( 'template', ['ngAria', 'ngRoute', 'ngSanitize']);
+var portfolio = angular.module( 'portfolio', ['ngAria', 'ngRoute', 'ngSanitize']);
 
 // ----------------------------------------------------------------------------
 // controller head
-template.controller( 'head', ['$scope', '$http', function( $scope, $http ) {
+portfolio.controller( 'head', ['$scope', '$http', function( $scope, $http ) {
     $http.get( 'portfolio/banner' ).then( function( res ) {
         if( res == 200 ) {
             $scope.title = res.data.title;
