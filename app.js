520770a954bd675c1966c52f4725f813bab29cf9
FlorentinDUBOIS <dubois.florentin@live.fr>
2016-05-04 18:26:18 +0200

    update

--- app.js
+++ app.js
@@ -14,6 +14,31 @@ portfolio.controller( 'head', ['$scope', '$http', function( $scope, $http ) {
 
 // ----------------------------------------------------------------------------
 // controller
+portfolio.controller( 'footer', ['$scope', '$http', function( $scope, $http ) {
+    $scope.date = (new Date()).getFullYear();
+
+    $http.get( 'user/public_name' ).then( function( res ) {
+        if( res.status == 200 ) {
+            $scope.name = res.data;
+        }
+    });
+
+    var url = ['banner', 'presentation', 'educations', 'experiences', 'skills', 'hobbies', 'contact'];
+    var get = function( url ) {
+        $http.get( 'portfolio/' + url ).then( function( res ) {
+            if( res.status == 200 ) {
+                $scope[url] = res.data;
+            }
+        });
+    };
+
+    for( var i in url ) {
+        get( url[i] );
+    }
+}]);
+
+// ----------------------------------------------------------------------------
+// controller
 portfolio.controller( 'banner', ['$scope', '$http', function( $scope, $http ) {
     $http.get( 'user/public_name' ).then( function( res ) {
         if( res.status == 200 ) {
@@ -21,9 +46,16 @@ portfolio.controller( 'banner', ['$scope', '$http', function( $scope, $http ) {
         }
     });
 
-    $http.get( 'portfolio/banner' ).then( function( res ) {
-        if( res.status == 200 ) {
-            $scope.banner = res.data;
-        }
-    });
+    var url = ['banner', 'presentation', 'educations', 'experiences', 'skills', 'hobbies', 'contact'];
+    var get = function( url ) {
+        $http.get( 'portfolio/' + url ).then( function( res ) {
+            if( res.status == 200 ) {
+                $scope[url] = res.data;
+            }
+        });
+    };
+
+    for( var i in url ) {
+        get( url[i] );
+    }
 }]);
